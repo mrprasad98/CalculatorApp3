b765c388711aeb9f99550d82830aba35409877f8
Rajendra <rajendraprasad.modem@gmail.com>
2026-04-29 22:12:08 +0530

    two

--- src/test/java/testNGPratice/Testing.java
+++ src/test/java/testNGPratice/Testing.java
@@ -3,13 +3,16 @@ package testNGPratice;
 import org.testng.annotations.Test;
 
 public class Testing {
-	
-	@Test
-	public void m1()
-	{
+
+	@Test(priority = 2)
+	public void m1() {
 		System.out.println("Hi");
-		
+
 	}
 
-	
+	@Test(priority = 1)
+	public void m2() {
+		System.out.println("Hello");
+	}
+
 }
